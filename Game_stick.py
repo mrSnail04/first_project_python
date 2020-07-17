bcfbfd97c9587f51dae1e8f00e77c49a5f5094f4
Maksim Bulauski <maks.bulavsky@gmail.com>
2020-07-17 17:39:50 +0300

    Upd game, add players name

--- Game_stick.py
+++ Game_stick.py
@@ -1,4 +1,4 @@
-number=int(input('Введите количесвто палочек'))
+number=int(input('Введите количество палочек\n'))
 p1=input('Имя первого игрока\n')
 p2=input('Имя второго игрокa\n')
 player_turn = p1
@@ -8,21 +8,22 @@ def switch_players (turn):
     return p1 if player_turn == p2 else p2
 def end_of_game(sticks):
     return number <=0
+
 while (not end_of_game(number)):
-    print(f'Сколько палочек хотите зять? Осталось {number}')
+    print(f'{player_turn}, сколько палочек хотите взять? Осталось {number}\n')
     taken=int(input())
     
     if not can_take_stick(taken) :
         print('Не правильно количесвто палочек')
         continue
     if number == 2 and taken == 3:
-        print (f'Нельзя взять больше {number} палочек')
+        print (f'{player_turn}, нельзя взять больше {number} палочек')
         continue
     if number == 1 and taken > 1:
-        print (f'Нельзя взять больше {number} палочек')
+        print (f'{player_turn}, нельзя взять больше {number} палочек')
         continue
     number -= taken
-    print(f'Взял {taken} палочек\n')
+    print(f'{player_turn} взял {taken} палочек\n')
     
     if end_of_game(number):
         print(f'{player_turn} проиграл')
