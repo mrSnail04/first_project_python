54d3f49e53697ff10ac91281133439a94f894fa2
Maksim Bulauski <maks.bulavsky@gmail.com>
2020-09-23 19:21:01 +0300

    semaphore_exemple upd

---
+++ Semaphore_night_club.py
@@ -0,0 +1,23 @@
+import threading
+import time
+
+
+class Nightclub:
+    def __init__(self):
+        self.bouncer= threading.Semaphore(3)
+
+    def open_club(self):
+        for x in range(1, 20):
+            t = threading.Thread(target=self.guest, args=[x])
+            t.start()
+    def guest(self, guest_id):
+        print(f"\nGuest {guest_id} is waiting to entering night club")
+        self.bouncer.acquire()
+        print(f"\nGuest {guest_id} is dancing")
+        time.sleep(1)
+        print(f"\nGuest {guest_id} is leaving the night club")
+        self.bouncer.release()
+
+if __name__ == "__main__":
+    club = Nightclub()
+    club.open_club()
