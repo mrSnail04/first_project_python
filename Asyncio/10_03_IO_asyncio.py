82a0651abe5fef36f16ffd5b9c6ed3d0dcc68c11
Maksim Bulauski <maks.bulavsky@gmail.com>
2020-10-26 19:12:23 +0300

    Threading IO vs Asyncio IO vs Sync IO

---
+++ Asyncio/10_03_IO_asyncio.py
@@ -0,0 +1,33 @@
+import asyncio
+
+import aiohttp
+
+from multithreding.decorators import async_measure_time
+
+
+async def download_site(url, session):
+    async with session.get(url) as response:
+        print(f'Read {response.content.total_bytes} from {url}')
+
+
+@async_measure_time
+async def download_all_sites(sites):
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for url in sites:
+            task = asyncio.create_task(download_site(url, session))
+            tasks.append(task)
+        try:
+            print('Before await')
+            await asyncio.gather(*tasks, return_exceptions=True)
+        except Exception as ex:
+            print(repr(ex))
+
+
+if __name__ == '__main__':
+    sites = [
+        'https://www.engineerspock.com/',
+        'https://enterprisecraftsmanship.com/'
+    ] * 80
+
+    asyncio.run(download_all_sites(sites))
